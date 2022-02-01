abbed0de47c9a241d4c27e7a16220114ff9f495c
Shankar Wagh <76767335+Shankar297@users.noreply.github.com>
2022-02-01 13:39:26 +0530

    file uploaded

--- db_connection.py
+++ db_connection.py
@@ -1,10 +1,10 @@
 from cassandra.cluster import Cluster
 from cassandra.auth import PlainTextAuthProvider
 
-SECURE_CONNECT_BUNDLE = r"C:\Users\Shankar Wagh SNR\Desktop\Data_Science_Course\Internship\I-NEURON\Astra DB\secure-connect-sample.zip"
-USERNAME = 'qBYUXirKQnEPmuXnraYUHHdJ'
-PASSWORD = 'mMSs.wRDJW7K7fjjKLGgup2bbv5ojh-l4SivkG78H0KDJBb6ituFaHxq6ahPzwmCvAN+cUQQlrKECTpFT+qYDQX0Wijv94Z4opyy7xKZgwLyWC5ktO2-M5,j9u48cmGz'
-KEYSPACE = 'sample'
+SECURE_CONNECT_BUNDLE = r""
+USERNAME = ''
+PASSWORD = ''
+KEYSPACE = ''
 
 class Connection:
     def __init__(self):
